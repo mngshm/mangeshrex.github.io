90bc27a4c743ef02490e08b7054662f892d1b74f
Mangeshrex <rex835138@gmail.com>
2022-10-22 10:27:19 +0530

    fix typo's and grammer

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+
+        import { TailwindConfig } from 'tailwindcss/tailwind-config';
+
+        const config: TailwindConfig = {
+  "content": [],
+  "theme": {
+    "extend": {}
+  }
+}
+
+        module.exports = config
+      
