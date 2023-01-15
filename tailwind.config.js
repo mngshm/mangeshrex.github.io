bdb67232d698048ae7bebe4c5a95b9493cfbc4e3
Mangeshrex <rex835138@gmail.com>
2023-01-15 10:17:24 +0530

    fix: bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const colors = require('tailwindcss/colors') 
 
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
@@ -14,23 +14,23 @@ module.exports = {
     extend: {
       colors: {
         white: {
-          100: "#e4e4e4",
-          200: "#dfdfdf",
-          300: "#dadada",
-          400: "#d5d5d5",
-          500: "#d0d0d0",
-          600: "#cbcbcb",
-          700: "#c6c6c6"
+          100: "#cacad9",
+          200: "#c5c5d4",
+          300: "#c0c0cf",
+          400: "#b9b9c8",
+          500: "#b2b2c1",
+          600: "#ababba",
+          700: "#a4a4b3"
         },
         black: "#0d0d0d",
         blue: {
-          100: "#67b0e8",
-          200: "#62abe3",
-          300: "#5da6de",
-          400: "#58a1d9",
-          500: "#539cd4",
-          600: "#4e97cf",
-          700: "#597eb0 ",
+          100: "#b0cce3",
+          200: "#a9c5dc",
+          300: "#a2bed5",
+          400: "#9bb7ce",
+          500: "#94b0c7",
+          600: "#8da9c0",
+          700: "#86a2b9",
         },
       },
     },
